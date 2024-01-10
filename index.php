eab93315e4c357de672e56dba6a8d7effcaa36b7
VitomsCatena <vitomscatenathe1@gmail.com>
2024-01-10 09:15:15 +0300

    updated html

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="my personal portfolio">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="author" content="Victor Omondi ,Vitoms Catena">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iamvictor</title>
+</head>
+<body>
+    
+</body>
+</html>
