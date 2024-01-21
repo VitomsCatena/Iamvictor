71975fd5dbc7b3710d50dbc9bfe7718ea94155a0
VitomsCatena <vitomscatenathe1@gmail.com>
2024-01-21 21:13:29 +0300

    htmlfile

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--meta tags-->
+      <meta charset="UTF-8">
+      <meta name="description" content="my personal portfolio">
+      <meta name="keywords" content="HTML, CSS, JavaScript">
+      <meta name="author" content="Victor Omondi ,Vitoms Catena">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--end of meta tags-->
+    <title>iamvictor</title>
+
+<!--fontawesome cdn link-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/
+font-awesome/6.1.0/css/all.min.css">
+
+<!--normal links-->
+<link rel="stylesheet" href="css/style.css">
+<!--end of normal links-->
+
+</head>
+<body>
+    
+<!--header-section--starts--here-->
+<header class="header">
+
+  <div id="menu-btn" class="fas fa-bars"></div>
+  
+  <a href="#home" class="logo">IAMVICTOR</a>
+
+  <nav class="navbar">
+    <a href="#home" class="active">Home</a>
+    <a href="#about">About</a>
+    <a href="#services">Services</a>
+    <a href="#portfolio">portfolio</a>
+    <a href="#contact">Contacts</a>
+  </nav>
+
+  <div class="follow">
+<a href="#" class="fab fa-facebook-f"></a>
+<a href="https://twitter.com/Afrikaan_dream" class="fab fa-x"></a> 
+<a href="https://www.instagram.com/stories/midnigh.t_5/" class="fab fa-instagram"></a>
+<a href="https://www.linkedin.com/in/victor-omondi-onyango/" class="fab fa-linkedin"></a>
+<a href="https://github.com/VitomsCatena" class="fab fa-github"></a>
+<a href="https://wa.me/254799802335?text=Hello%20am%20intrested%20in%20some%20of%20your%20services%20%0A" class="fab fa-whatsapp"></a>
+<a href="https://discord.com/channels/@me" class="fab fa-discord"></a>
+<a href="https://www.youtube.com/channel/UCCJSr-IRfXWXpj6swhbsGnQ" class="fab fa-youtube"></a>
+  </div>
+</header>
+<!--header-section--ends--here-->
+
+<!--Home-section-begins-here-->
+<section class="home" id="home">
+
+   <div class="images">
+    <img src="Images/IMG-20220805-WA0029.jpg" alt="picture">
+   </div>
+    <div class="content">
+      <h3>Hello!</h3>
+      <h3>I am Victor Omondi</h3>
+      <span>Web designer , developer & a software programmer.</span>
+      <p>Pride Myself On Having Solid Relational Abilities, Tender Loving Care,
+       The Capacity To Perform Multiple Tasks, And Unwavering Quality. I Am Proactive, Dedicated,</p>
+        <a href="#about" class="btn">about me</a>
+    </div>
+</section>
+
+<!--Home--section-ends-here-->
+
+<!--About-section-starts-here-->
+<section class="about" id="about">
+
+  <h1 class="heading"><span>Biography</span></h1>
+  
+  <div class="biography">
+  <p>Hello, I'm Victor Omondi, a web designer based in Nairobi,Kenya.
+    With 2 years of experience in tech, I specialize in front end development.</p>
+
+      <div class="bio">
+        <h3> <span>Name :</span> Victor Omondi</h3>
+        <h3> <span>Email :</span> <a href="mailto:vitomscatenathe1@gmail.com">vitomscatenathe1@gmail.com</a></h3>
+        <h3> <span>Address :</span> Nairobi, Kenya</h3>
+        <h3> <span>Phone :</span> +254799802335</h3>
+        <h3> <span>Age :</span> 21years</h3>
+        <h3> <span>experience :</span> 2+ years of experience</h3>
+      </div>
+      <a href="#" class="btn">Download Cv</a>
+    </div>
+ <div class="skills">
+
+     <h1 class="heading"><span>Skills</span></h1> 
+     <div class="progress">
+        <div class="bar"> <h3><span>HTML</span><span>96%</span></h3> </div>   
+        <div class="bar"> <h3><span>CSS</span><span>80%</span></h3> </div>
+        <div class="bar"> <h3><span>Bootstrap</span><span>99%</span></h3> </div>
+        <div class="bar"> <h3><span>JavaScript</span><span>36%</span></h3> </div>
+        <div class="bar"> <h3><span>PHP</span><span>86%</span></h3> </div>
+        <div class="bar"> <h3><span>Python</span><span>55%</span></h3> </div>
+        <div class="bar"> <h3><span>MySQL</span><span>60%</span></h3> </div>
+        <div class="bar"> <h3><span>Wordpress</span><span>55%</span></h3> </div>
+
+     </div>
+
+ </div>
+
+ <div class="edu-exp">
+
+     <h1 class="heading"><span>Education and experience</span></h1>
+
+     <div class="row">
+           <div class="box-container">
+             <h3 class="title">Education</h3>
+              <div class="box">
+                <span>(2020 -2021)</span>
+                <h3>Web designer</h3>
+                <p>web design journey started here.</p>
+              </div>
+
+              <div class="box">
+                <span>(2021 -2022)</span>
+              <h3>Web development</h3>
+                <p>web design journey started here.</p>
+              </div>
+
+              <div class="box">
+                <span>(2022 -2023)</span>
+                <h3>Web designer</h3>
+                <p>web design journey started here.</p>
+              </div>
+
+           </div>
+
+           <div class="box-container">
+             <h3 class="title">experience</h3>
+              <div class="box">
+                <span>(2020 -2021)</span>
+                <h3>front end development</h3>
+                <p>web design journey started here.</p>
+              </div>
+
+              <div class="box">
+                <span>(2021 -2022)</span>
+              <h3>System management</h3>
+                <p>web design journey started here.</p>
+              </div>
+
+              <div class="box">
+                <span>(2022 -2023)</span>
+                <h3>It administaror</h3>
+                <p>web design journey started here.</p>
+              </div>
+              
+           </div>
+
+     </div>
+ </div>
+
+
+</section>
+<!--end-of-about-section-->
+
+<!--service section starts here-->
+
+<section class="services" id="services">
+
+<h1 class="heading"><span>services</span></h1>
+
+     <div class="box-container">
+
+      <div class="box">
+
+   <i class="fas fa-code"></i>
+     <h3>Web development.</h3>
+     <p>From designing and developing custom websites to optimizing existing ones for better performance, 
+    I work closely with clients to understand their unique needs and deliver solutions that meet their goals. </p>
+
+      </div>
+
+    <div class="box">
+   <i class="fas fa-phone"></i>
+     <h3>Customer Support</h3>
+     <p>I also offer customer support to ensure that your online presence is 
+      not only visually appealing and functional but also well-maintained and responsive to 
+      your customers' needs.</p>
+
+     </div>
+
+<div class="box">
+   <i class="fas fa-bootstrap"></i>
+     <h3>Bootstrap.</h3>
+     <p>Due to it's flexibility, ease of use, and time-saving benefits 
+      and the valuable toolkit for creating modern and visually
+       appealing web applications.Am also proficient in it.
+</p>
+
+     </div>
+
+
+     <div class="box">
+   <i class="fas fa-bullhorn"></i>
+     <h3>Digital Marketing</h3>
+     <p>Digital marketing has become increasingly important in recent years as more and
+       more people use the internet to research and purchase products and services.I use email marketing, 
+       social media marketing, search engine optimization, and digital advertising to deliver my services.</p>
+
+     </div>
+
+<div class="box">
+   <i class="fas fa-phone"></i>
+     <h3>Virtual Assistance</h3>
+     <p>I provides remote administrative, clerical, and support services to businesses and individuals.
+       I do various tasks, such as scheduling, data entry, converting files, fielding phone calls, and coordinating meetings.
+      I also specialized in services like skip tracing or customer support assistance.
+     </p>
+
+     </div>
+
+        <div class="box">
+   <i class="fab fa-wordpress"></i>
+     <h3>Wordpress.</h3>
+     <p>I also create and manage blogs using Wordpress 
+      Since it offers a flexible and scalable solution for your web development needs.Am also ready to deliver.</p>
+     </div>
+
+     </div>
+
+</section>
+
+<!--service section end-->
+
+             <!--Start of portfolio section-->
+<section class="portfolio" id="portfolio">
+
+<h1 class="heading"><span>Portfolio</span></h1>
+
+   <div class="box-container">
+         <div class="box">
+  <img src="Images/logo (1).png" width="400px" height="300px" alt="">
+  <h3>Web designer.</h3>
+   <span>(2020-2021)</span>
+         </div>
+
+<div class="box">
+  <img src="Images/Neon-lsvi.jpg" width="400px" height="300px" alt="">
+  <h3>Web designer.</h3>
+   <span>(2020-2021)</span>
+         </div>
+
+<div class="box">
+  <img src="Images/vitoms catena.png" width="400px" height="300px" alt="">
+  <h3>Web designer.</h3>
+   <span>(2020-2021)</span>
+         </div>
+
+<div class="box">
+  <img src="Images/vitoms.grafiti.jpg" width="400px" height="300px" alt="">
+  <h3>Web designer.</h3>
+   <span>(2020-2021)</span>
+         </div>
+
+<div class="box">
+  <img src="Images/logo (1).png" width="400px" height="300px" alt="">
+  <h3>Web designer.</h3>
+   <span>(2020-2021)</span>
+         </div>
+
+<div class="box">
+  <img src="Images/Neon-lsvi.jpg" width="400px" height="300px" alt="">
+  <h3>Web designer.</h3>
+   <span>(2020-2021)</span>
+         </div>
+
+   </div>
+
+</section>
+
+            <!--end of portfolio section-->
+<!--contact section starts here-->
+<section class="contact" id="contact">
+<h1 class="heading"><span>Contact me</span></h1>
+
+   <form action="" method="post">
+        <div class="flex">
+          <input type="text" name="name" placeholder="Input your name" class="box" required>
+          <input type="email" name="email" placeholder="enter email" class="box" required>
+        </div>
+        <input type="number" min="0" maxlength="15"   placeholder="Enter your number" class="box" required>
+        <textarea name="Message" class="box" required  placeholder="your message "      cols="30" rows="10"></textarea>
+   <input type="submit" value="send message" name="send" class="btn">
+      </form>
+
+</section>
+<!--contact section ends here-->
+
+<div class="credit"> &copy; copyright @ <?php echo date('Y'); ?> by <span>Vitor Omondi</span></div>
+
+
+
+
+</body>
+
+     <!--custom javascript link-->
+
+  <script src="JS/script.js"></script>
+
+      <!---end--of--custom-link-->
+
+</html>
