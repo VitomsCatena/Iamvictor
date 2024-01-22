dbb17d6dbaa0ba25d70e85de310ecd2b393b1600
VitomsCatena <vitomscatenathe1@gmail.com>
2024-01-22 17:17:14 +0300

    js

--- JS/script.js
+++ JS/script.js
@@ -20,6 +20,8 @@ if (window.innerWidth < 991){
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
-    if (top >= offset && top < offset + )
+    if (top >= offset && top < offset + height){
+      
+    }
   });
 }
