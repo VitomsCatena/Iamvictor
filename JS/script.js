e7fe20145cb5ec53b4c3a673764b07f1055e2eb4
VitomsCatena <vitomscatenathe1@gmail.com>
2024-01-22 17:15:20 +0300

    script

--- JS/script.js
+++ JS/script.js
@@ -18,5 +18,8 @@ if (window.innerWidth < 991){
     let top = window.scrollY;
     let offset = sec.offsetTop;
     let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    
   });
 }
